312b43d69ebce523e4f47cdc3961dbf667828a35
B-the-Reed <b.richard.reed@gmail.com>
2023-04-18 06:37:35 -0600

    merged enriched dataset with API data and saved in new CSV

--- config_br.py
+++ config_br.py
@@ -1,4 +1,2 @@
 # Geoapify API Key
-brandon_geoapify_key = "8197c8d7f0e44683b0fa23eb2e7c1a63"
-
-emma_geoapify_key = "c3b2764dfff24a979a0e86be3fd8a07b"
+geoapify_key = "8197c8d7f0e44683b0fa23eb2e7c1a63"
